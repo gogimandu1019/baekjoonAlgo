1a2068cb2d39d069de7278e4564abe636ba92d78
gogimandu1019 <indigosky94@naver.com>
2022-10-02 23:04:31 +0900

    20221002

--- src/programmers/Solution.java
+++ src/programmers/Solution.java
@@ -1,40 +1,19 @@
 package programmers;
 import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
         int max = 0;
         int count = 0;
-        //int index = 0;
         List<Integer> list = new LinkedList<>();
 
-        //최댓값 구하기
-        for(int a : arr){
-            if (a > max){
-                max = a;
-            }
-        }
-        //최댓값 개수
-        for(int a : arr){
-            if(a == max){
-                count++;
-            }
-        }
 
-        //정답 배열에 인덱스 채우기
-        //answer = new int[count];
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == max){
-                list.add(i);
-            }
-        }
-        //리스트를 배열로 변환
-        answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = list.get(i);
-        }
+        max = Arrays.stream(arr).max().getAsInt();    //optionalInt
 
-        return answer;
+
+        int finalMax = max;
+        return  IntStream.range(0, arr.length).filter(i ->arr[i] == finalMax).toArray();
     }
 }
